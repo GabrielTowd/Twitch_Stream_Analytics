e53651e6eb7a7fd74363084a549dbad331ee1cce
Gabriel <utilateurmacbookpro@MacBook-Pro-de-Gabriel.local>
2020-03-19 18:44:24 +0100

    save on json file + get game name from command line

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 import json
 import requests
 import datetime
+import sys
 
 api_token = 'ek4ezzhg4t9m6t4wx3weml4s86cyz7'
 api_url_base = 'https://api.twitch.tv/helix'
 api_header = {'Content-Type': 'application/json',
            'Client-ID': api_token}
+game_name = sys.argv[1]
 
 def get_game_id(game):
     response = requests.get(api_url_base + '/games?name=' + game, headers=api_header)
@@ -40,16 +42,17 @@ def get_streams_datas(game_id):
             top_twenty.append(top_streamer)
     
     data = {
-        "view_count": view_count,
+        "views_count": views_count,
         "top_twenty": top_twenty,
         "streamer_count": streamer_count
     }
 
     date = datetime.datetime.now()
 
-    with open(date + ////game////, 'w') as write_file:
+    with open(str(date.year) + '-' + str(date.month) + '-' + str(date.day) + '-' + str(date.hour) + '-' + game_name + '.json', 'w') as write_file:
         json.dump(data, write_file)
 
-
-game_id = get_game_id("Dofus")
-get_streams_datas(game_id)
+if __name__ == "__main__":
+    game_id = get_game_id(game_name)
+    get_streams_datas(game_id)
+    pass
